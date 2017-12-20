00ed972d164a2e80e796f4af9d67d35d423dbbbb
Gerardo <gerardolopezduenas@gmail.com>
2017-12-19 22:05:47 -0600

    Added Interpolation Search algorithm

---
+++ search/interpolation_search.c
@@ -0,0 +1,36 @@
+#include "interpolation_search.h"
+#include <stdio.h>
+
+/**
+ * Search for a number in a sorted list of integers
+ * @param  list   list of integers
+ * @param  target number to search
+ * @param  size   length of the list
+ * @return        index of target
+ */
+int search(int* list, int target, int n) {
+    // Indexes of the corners
+    int lowCorner = 0, highCorner = (n - 1);
+
+    // Since array is sorted, the element
+    // must be in range defined by corner
+    while (
+    	lowCorner <= highCorner &&
+    	target >= list[lowCorner] &&
+    	target <= list[highCorner]) {
+        int pos = lowCorner + (( (float)(highCorner-lowCorner) /
+              (list[highCorner]-list[lowCorner]) ) * (target - list[lowCorner]));
+
+        // Target found
+        if (list[pos] == target)
+            return pos;
+
+        // If target is larger, target is in upper part
+        if (list[pos] < target)
+            lowCorner = pos + 1;
+        // If target is smaller, target is in lower part
+        else
+            highCorner = pos - 1;
+    }
+    return -1;
+}
---
+++ search/interpolation_search.h
@@ -0,0 +1,4 @@
+#ifndef INTERPOLATION_SEARCH_H_
+#define INTERPOLATION_SEARCH_H_
+	int search(int* list, int target, int n);
+#endif
